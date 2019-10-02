01f147163ce61189db6b7d11054351ab618eeca2
ericXin95616 <xinbochao97@outlook.com>
2019-10-02 08:16:29 +0800

    Program for Phrase 1-Running code in the hard way(Bug not fixed).

--- sshell.c
+++ sshell.c
@@ -1,13 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <zconf.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
         char *cmd = "/bin/date -u";
-        int retval;
+        char *originStr = malloc(100);
+        strcpy(originStr, cmd);
+        char **args = malloc(100);
 
-        retval = system(cmd);
-        fprintf(stdout, "Return status value for '%s': %d\n", cmd, retval);
+        args[0] = strtok(originStr, " ");
+
+        args[1] = strtok(NULL, " ");
+
+        int pid = fork();
+        int status;
+
+        if(pid == 0)
+            execl(args[0], args[1], (char*) NULL);
+
+        waitpid(pid, &status, 0);
+        fprintf(stdout, "Return status value for '%s': %d\n", cmd, WIFEXITED(status));
 
         return EXIT_SUCCESS;
 }
