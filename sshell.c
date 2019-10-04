79d2a9429dc9e2bbbefe5c4460afcf93b5c7031a
ericXin95616 <xinbochao97@outlook.com>
2019-10-04 16:55:56 +0800

    Phrase 4(almost finished, not test yet)

--- sshell.c
+++ sshell.c
@@ -7,71 +7,182 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <assert.h>
+#include <ctype.h>
 
 #define MAX_SIZE 512
-
-enum {
-    TOO_MANY_ARGS;
-    NO_DIR; // EXEC
-    NO_CMD; // EXEC
-    MISSING_CMD;
-    MISLOCATE_INPUT;
-    NO_INPUT_FILE;
-    CANT_OPEN_INPUT_FILE;
-    MISTLOCATE_OUTPUT;
-    NO_OUTPUT_FILE;
-    CANT_OPEN_OUTPUT_FILE;
-    MISLOCATE_BACKGROUND_SIGN;
-    ACTIVE_JOBS_NOT_FINISH; //EXEC
-};
 
 typedef struct command {
     char **args; //array of argument, args[0] is filename
     char *inputfile;
     char *outputfile;
+    bool background;
+    bool first_element; // allow input redirect
+    bool last_element; // allow output redirect
     int argnum;
-    int ordernum;
-    int totalcmd;
 } singalcmd;
 
 /*
  * Free memory allocate for each singalcmd object
  */
-void _myfree(singalcmd cmd) {
-    if(cmd.inputfile)
-        free(cmd.inputfile);
-    if(cmd.outputfile)
-        free(cmd.inputfile);
+void _myfree(singalcmd *cmd) {
+    if(cmd->inputfile)
+        free(cmd->inputfile);
+    if(cmd->outputfile)
+        free(cmd->inputfile);
 
-    assert(cmd.args);
-    if(cmd.args) {
+    assert(cmd->args);
+    if(cmd->args) {
         int i = 0;
-        while(cmd.args[i]){
-            free(cmd.args[i]);
+        while(cmd->args[i]){
+            free(cmd->args[i]);
             ++i;
         }
     }
-    free(cmd.args);
+    free(cmd->args);
 }
 
 /*
  * Free memory allocate for an array of singalcmd
  * singalcmd **cmds? Care of Memory issue!
  */
-void myfree(singalcmd *cmds, int cmdnum) {
-    for (int i = 0; i < cmdnum; ++i)
+void myfree(singalcmd **cmds, int cmdnum)
+{
+    for (int i = 0; i < cmdnum; ++i) {
         _myfree(cmds[i]);
-    free(cmds);
+        cmds[i] = NULL;
+    }
 }
 
-/*
- * This function is for build and check
- * initialize singal command
- * If input command string is not valid,
- * we output error message.
- */
-bool initialize_singal_command(singalcmd *cmd, int ordernum, int totalnum) {
+bool ischar(const char a, const char * list)
+{
+    if(!list)
+        return false;
 
+    for (int i = 0; i < strlen(list); ++i){
+        if(list[i] == a)
+            return true;
+    }
+    return false;
+}
+
+bool check_command_validity(char *inputfile, char *outputfile, bool first_element, bool last_element, int argnum) {
+    if(!first_element && inputfile){
+        printf("Error: mislocated input redirection\n");
+        return false;
+    }
+
+    if(!last_element && outputfile){
+        printf("Error: mislocated output redirection\n");
+        return false;
+    }
+
+    if(inputfile && !fopen(inputfile, "r")){
+        printf("Error: cannot open input file\n");
+        return false;
+    }
+
+    if(outputfile && !fopen(outputfile, "r")){
+        printf("Error: cannot open output file\n");
+        return false;
+    }
+    return true;
+}
+
+void initialize_command(singalcmd **cmd, char **args, char *inputfile, char *outputfile,
+                        bool background, bool first_element, bool last_element, int argnum)
+{
+    (*cmd) = malloc(sizeof(singalcmd));
+    (*cmd)->background = background;
+    (*cmd)->first_element = first_element;
+    (*cmd)->last_element = last_element;
+    (*cmd)->argnum = argnum;
+
+    (*cmd)->inputfile = malloc((strlen(inputfile) + 1) * sizeof(char));
+    strcpy((*cmd)->outputfile, outputfile);
+    (*cmd)->outputfile = malloc((strlen(outputfile) + 1) * sizeof(char));
+    strcpy((*cmd)->outputfile, outputfile);
+
+    (*cmd)->args = malloc(argnum * sizeof(char*));
+    for (int i = 0; i < argnum; ++i) {
+        (*cmd)->args[i] = malloc((strlen(args[i]) + 1) * sizeof(char));
+        strcpy((*cmd)->args[i], args[i]);
+    }
+}
+
+int get_next_nonspace_char_pos(const char *src, int index)
+{
+    while(isspace(src[++index]));
+    return index;
+}
+
+int get_prev_nonspace_char_pos(const char *src, int index)
+{
+    while(isspace(src[--index]));
+    return index;
+}
+
+bool check_vertical_bar(const char *src, int index)
+{
+    int prev = get_prev_nonspace_char_pos(src, index);
+    int next = get_next_nonspace_char_pos(src, index);
+
+    if(prev < 0 || next >= strlen(src)){
+        printf("Error: missing command\n");
+        return false;
+    }
+
+    if(src[prev] == '|' || ischar(src[next], "|&<>")){
+        printf("Error: missing command\n");
+        return false;
+    }
+
+    return true;
+}
+
+bool check_input_sign(const char *src, int index)
+{
+    int prev = get_prev_nonspace_char_pos(src, index);
+    int next = get_next_nonspace_char_pos(src, index);
+
+    if(prev < 0){
+        printf("Error: missing command\n");
+        return false;
+    }
+
+    if(next >= strlen(src) || ischar(src[next], "|&<>")){
+        printf("Error: no input file\n");
+        return false;
+    }
+
+    return true;
+}
+
+bool check_output_sign(const char *src, int index)
+{
+    int prev = get_prev_nonspace_char_pos(src, index);
+    int next = get_next_nonspace_char_pos(src, index);
+
+    if(prev < 0) {
+        printf("Error: missing command\n");
+        return false;
+    }
+
+    if(next >= strlen(src) || ischar(src[next], "|&<>")){
+        printf("Error: no output file\n");
+        return false;
+    }
+
+    return true;
+}
+
+bool check_ampersand(const char *src, int index) {
+    int next = get_next_nonspace_char_pos(src, index);
+
+    if(next >= strlen(src))
+        return true;
+
+    printf("Error: mislocated background sign\n");
+    return false;
 }
 
 /*
@@ -80,33 +191,106 @@ bool initialize_singal_command(singalcmd *cmd, int ordernum, int totalnum) {
  * we print error message and return false.
  * If source string is vaild,
  * we divide it into different commands and
- * call initialize_singal_command function to
+ * call check_and_init_cmd function to
  * build cmds
  */
-bool parse_src_string(const char *src, singalcmd *cmds, int *cmdnum)
+bool parse_src_string_and_build_cmds(const char *src, singalcmd **cmds, int *cmdnum)
 {
-    char *dupcmd = malloc((strlen(src)+1) * sizeof(char));
-    char *delimiters = "|";
-    char *saveptr;
-    char **tmp = malloc(strlen(src) * sizeof(char*));
-    strcpy(dupcmd, src);
+    char *delimiters = "|&<>";
+
+    char *inputfile = malloc(strlen(src) * sizeof(char));
+    char *outputfile = malloc(strlen(src) * sizeof(char));
+
+    char *arg = malloc((strlen(src) + 1) * sizeof(char));
+
+    char **args = malloc(strlen(src) * sizeof(char*));
+    for (int i = 0; i < strlen(src); ++i)
+        args[i] = malloc((strlen(src)+1) * sizeof(char));
 
     (*cmdnum) = 0;
-    for (char *token = strtok_r(dupcmd, delimiters, &saveptr); token != NULL ; token = strtok_r(NULL, delimiters, &saveptr)) {
-        tmp[(*cmdnum)] = token;
-        ++(*cmdnum);
-    }
+    int _argsindex = 0;
+    int preindex = -1;
+    int argindex = 0;
+    bool input_redirect = false;
+    bool output_redirect = false;
+    bool background = false;
 
-    cmds = malloc((*cmdnum) * sizeof(singalcmd));
-    for (int i = 0; i < *cmdnum; ++i) {
-        if(initialize_singal_command(&cmds[i], i, *cmdnum)) {
-            myfree(cmds, *cmdnum);
-            free(dupcmd);
-            return false;
+    while(preindex < strlen(src)) {
+        int index = get_next_nonspace_char_pos(src, preindex);
+        if(index >= strlen(src))
+            break;
+
+        if(((index > preindex + 1) || ischar(src[index], delimiters)) && argindex != 0){
+            arg[argindex] = '\0';
+
+            if(input_redirect){
+                strcpy(inputfile, arg);
+                input_redirect = false;
+            } else if(output_redirect){
+                strcpy(outputfile, arg);
+                output_redirect = false;
+            }else {
+                strcpy(args[_argsindex], arg);
+                ++_argsindex;
+            }
+
+            memset(arg, 0, (strlen(src) + 1) * sizeof(char));
+            argindex = 0;
+        } else if((index == preindex + 1) && !ischar(src[index], delimiters)){
+            arg[argindex] = src[index];
+            ++argindex;
         }
+
+        switch(src[index]) {
+            case '|':
+                if(!check_vertical_bar(src, index))
+                    return false;
+                if(!check_command_validity(inputfile, outputfile, (*cmdnum) == 0, false, _argsindex))
+                    return false;
+                initialize_command(&(cmds[*cmdnum]), args, inputfile, outputfile, background, (*cmdnum) == 0, false, _argsindex);
+                // back to the origin state
+                ++(*cmdnum);
+                _argsindex = 0;
+                input_redirect = false;
+                output_redirect = false;
+                background = false;
+                memset(inputfile, 0, strlen(src) * sizeof(char));
+                memset(outputfile, 0, strlen(src) * sizeof(char));
+                for (int i = 0; i < strlen(src); ++i)
+                    memset(args[i], 0, (strlen(src)+1) * sizeof(char));
+                break;
+            case '<':
+                if(!check_input_sign(src, index))
+                    return false;
+                input_redirect = true;
+                break;
+            case '>':
+                if(!check_output_sign(src, index))
+                    return false;
+                output_redirect = true;
+                break;
+            case '&':
+                if(!check_ampersand(src, index))
+                    return false;
+                background = true;
+                break;
+            default:
+                break;
+        }
+        preindex = index;
     }
 
-    free(dupcmd);
+    if(!check_command_validity(inputfile, outputfile, (*cmdnum) == 0, true, _argsindex))
+        return false;
+    initialize_command(&(cmds[*cmdnum]), args, inputfile, outputfile, background, (*cmdnum) == 0, true, _argsindex);
+    ++(*cmdnum);
+
+    free(inputfile);
+    free(outputfile);
+    for (int i = 0; i < strlen(src); ++i)
+        free(args[i]);
+    free(args);
+
     return true;
 }
 
@@ -115,7 +299,7 @@ int main(int argc, char *argv[])
     char *src = malloc(MAX_SIZE * sizeof(char)); // FREE
     //char **args = malloc(MAX_SIZE * sizeof(char*)); // FREE
 
-    singalcmd *cmds = NULL;
+    singalcmd **cmds = malloc(MAX_SIZE * sizeof(singalcmd*));
     int cmdnum;
 
     do {
@@ -127,19 +311,19 @@ int main(int argc, char *argv[])
             continue;
         src[charsnum-1] = '\0'; // delete trailing newline char
 
-        if(!parse_src_string(src, cmds, &cmdnum))
+        if(!parse_src_string_and_build_cmds(src, cmds, &cmdnum))
             continue;
 
         assert(cmds);
         for (int i = 0; i < cmdnum; ++i) {
-            if(strcmp(cmds[i].args[0], "exit") == 0)
+            if(strcmp(cmds[i]->args[0], "exit") == 0)
                 goto label;
 
             int pid = fork();
             int status;
 
             if (pid == 0)
-                execvp(cmds[i].args[0], cmds[i].args);
+                execvp(cmds[i]->args[0], cmds[i]->args);
             else {
                 wait(&status);
                 fprintf(stderr, "Return status value for '%s': %d\n", src, WEXITSTATUS(status));
@@ -157,6 +341,7 @@ int main(int argc, char *argv[])
     } while(true);
 
     free(src);
+    free(cmds);
 
     fprintf(stderr, "Bye...\n");
     return EXIT_SUCCESS;
