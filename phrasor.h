8f270d731e97f22b0ec09598f9e90cb4a1f294c1
ericXin95616 <xinbochao97@outlook.com>
2019-10-06 10:31:36 +0800

    Phrase4(Finished)

---
+++ phrasor.h
@@ -0,0 +1,44 @@
+//
+// Created by xinbochao on 19-10-6.
+//
+
+#ifndef SHELL_PHRASOR_H
+#define SHELL_PHRASOR_H
+
+#include <stdbool.h>
+#define MAX_ARGUMENT_NUM 16
+
+typedef struct command {
+    char **args; //array of argument, args[0] is filename
+    char *inputfile;
+    char *outputfile;
+    bool background;
+    struct command *prev;
+    struct command * next;
+    int status;
+} command;
+
+bool parse_src_string(const char *cmd, command **header);
+void myfree(command *header);
+
+/* These are all the auxiliary functions:
+ *
+bool handle_vertical_bar_and_null(command **header, command **iter, const char *cmd,
+                                  char **args, char *inputfile, char *outputfile, int *index);
+bool check_vertical_bar(const char *src, int index);
+
+bool handle_ampersand(command **header, command **iter, const char *cmd, char **args,
+                      char *inputfile, char *outputfile, int index);
+bool check_ampersand(const char *src, int index);
+
+bool handle_redirect_sign(command **header, const char *cmd, char *token, char special, const char *delimiters,
+                          int *index, char *inputfile, char *outputfile);
+bool check_redirect_sign(command **header, const char *cmd, char *token, const char *delimiters, int *index, char special);
+
+char my_strtok(const char *src, char *dest, const char *delimiters, int *index);
+bool write_back(char **args, char *token);
+void clear_mem(char **args, char *inputfile, char *outputfile, char *token, command **header);
+void initialize_command(command **header, char **args, char *inputfile, char *outputfile, bool background,
+                        command *prev, command *next);
+ */
+#endif //SHELL_PHRASOR_H
--- sshell.c
+++ sshell.c
@@ -8,341 +8,159 @@
 #include <unistd.h>
 #include <assert.h>
 #include <ctype.h>
+#include <libgen.h>
+#include "phrasor.h"
 
 #define MAX_SIZE 512
 
-typedef struct command {
-    char **args; //array of argument, args[0] is filename
-    char *inputfile;
-    char *outputfile;
-    bool background;
-    bool first_element; // allow input redirect
-    bool last_element; // allow output redirect
-    int argnum;
-} singalcmd;
+/*
+ * call getcwd to get current working directory
+ * Notice that getcwd will allocate memory if success!
+ * so we must free it at the end
+ */
+void execute_pwd(command *cmd) {
+    char *currentDir = NULL;
+    currentDir = getcwd(currentDir, MAX_SIZE * sizeof(char));
+
+    cmd->status = (currentDir == NULL);
+    if(currentDir) {
+        printf("%s\n", currentDir);
+        free(currentDir);
+    }
+}
 
 /*
- * Free memory allocate for each singalcmd object
+ * filename provided can be relative path,
+ * we want to modify it to get absolut path
+ * Notice that we do not need to implement
+ * "cd -", "cd ~" commands.
  */
-void _myfree(singalcmd *cmd) {
-    if(cmd->inputfile)
-        free(cmd->inputfile);
-    if(cmd->outputfile)
-        free(cmd->inputfile);
+char * get_dest_dir(char *destDir, const char *filename){
+    destDir = getcwd(destDir, MAX_SIZE * sizeof(char));
+    // if filename is absolute path, nothing need to be done
+    if(filename[0] == '/') {
+        strcpy(destDir, filename); // dont directly return filename, it will cause mem_leak
+        return destDir;
+    }
+    // for relative path, modify it according to different situations
+    if(strcmp(filename, "..") == 0) {
+        destDir = dirname(destDir);
+    } else if(filename[0] == '.') {
+        strcat(destDir, &filename[1]);
+    } else {
+        strcat(destDir, "/");
+        strcat(destDir, filename);
+    }
+    return  destDir;
+}
 
-    assert(cmd->args);
-    if(cmd->args) {
-        int i = 0;
-        while(cmd->args[i]){
-            free(cmd->args[i]);
-            ++i;
+/*
+ * first call get_dest_dir to get absolute path
+ * and chdir. Remember to free destDir!
+ */
+void execute_cd(command *cmd) {
+    // args[0] is "cd", args[1] should be filename
+    char *destDir = NULL;
+    destDir = get_dest_dir(destDir, cmd->args[1]);
+
+    int returnVal = chdir(destDir);
+    free(destDir);
+    // 0 indicates success
+    if(!returnVal) {
+        cmd->status = returnVal;
+        return;
+    }
+    // returnVal -1 if fail
+    fprintf(stderr, "Error: no such directory\n");
+    cmd->status = 1; // set status to 1 indicates error
+}
+
+/*
+ * Execute commands stored in linked list header
+ * For every command, we first exam whether it is bulletin command
+ * If it is, execute them accordingly.
+ * If it is not, call execvp to execute them.
+ * Notice that when command is exit, we need to free memory and exit right away!
+ */
+void execute_commands(command *header, char *src) {
+    for (command *iter = header; iter != NULL; iter = iter->next) {
+        if(strcmp(iter->args[0], "exit") == 0) {
+            free(header);
+            free(src);
+            fprintf(stderr, "Bye...\n");
+            exit(EXIT_SUCCESS);
+        } else if (strcmp(iter->args[0], "cd") == 0) {
+            execute_cd(iter);
+            continue;
+        } else if (strcmp(iter->args[0], "pwd") == 0) {
+            execute_pwd(iter);
+            continue;
         }
-    }
-    free(cmd->args);
-}
 
-/*
- * Free memory allocate for an array of singalcmd
- * singalcmd **cmds? Care of Memory issue!
- */
-void myfree(singalcmd **cmds, int cmdnum)
-{
-    for (int i = 0; i < cmdnum; ++i) {
-        _myfree(cmds[i]);
-        cmds[i] = NULL;
-    }
-}
+        int status;
+        int pid = fork();
 
-bool ischar(const char a, const char * list)
-{
-    if(!list)
-        return false;
-
-    for (int i = 0; i < strlen(list); ++i){
-        if(list[i] == a)
-            return true;
-    }
-    return false;
-}
-
-bool check_command_validity(char *inputfile, char *outputfile, bool first_element, bool last_element, int argnum) {
-    if(!first_element && inputfile){
-        printf("Error: mislocated input redirection\n");
-        return false;
-    }
-
-    if(!last_element && outputfile){
-        printf("Error: mislocated output redirection\n");
-        return false;
-    }
-
-    if(inputfile && !fopen(inputfile, "r")){
-        printf("Error: cannot open input file\n");
-        return false;
-    }
-
-    if(outputfile && !fopen(outputfile, "r")){
-        printf("Error: cannot open output file\n");
-        return false;
-    }
-    return true;
-}
-
-void initialize_command(singalcmd **cmd, char **args, char *inputfile, char *outputfile,
-                        bool background, bool first_element, bool last_element, int argnum)
-{
-    (*cmd) = malloc(sizeof(singalcmd));
-    (*cmd)->background = background;
-    (*cmd)->first_element = first_element;
-    (*cmd)->last_element = last_element;
-    (*cmd)->argnum = argnum;
-
-    (*cmd)->inputfile = malloc((strlen(inputfile) + 1) * sizeof(char));
-    strcpy((*cmd)->outputfile, outputfile);
-    (*cmd)->outputfile = malloc((strlen(outputfile) + 1) * sizeof(char));
-    strcpy((*cmd)->outputfile, outputfile);
-
-    (*cmd)->args = malloc(argnum * sizeof(char*));
-    for (int i = 0; i < argnum; ++i) {
-        (*cmd)->args[i] = malloc((strlen(args[i]) + 1) * sizeof(char));
-        strcpy((*cmd)->args[i], args[i]);
-    }
-}
-
-int get_next_nonspace_char_pos(const char *src, int index)
-{
-    while(isspace(src[++index]));
-    return index;
-}
-
-int get_prev_nonspace_char_pos(const char *src, int index)
-{
-    while(isspace(src[--index]));
-    return index;
-}
-
-bool check_vertical_bar(const char *src, int index)
-{
-    int prev = get_prev_nonspace_char_pos(src, index);
-    int next = get_next_nonspace_char_pos(src, index);
-
-    if(prev < 0 || next >= strlen(src)){
-        printf("Error: missing command\n");
-        return false;
-    }
-
-    if(src[prev] == '|' || ischar(src[next], "|&<>")){
-        printf("Error: missing command\n");
-        return false;
-    }
-
-    return true;
-}
-
-bool check_input_sign(const char *src, int index)
-{
-    int prev = get_prev_nonspace_char_pos(src, index);
-    int next = get_next_nonspace_char_pos(src, index);
-
-    if(prev < 0){
-        printf("Error: missing command\n");
-        return false;
-    }
-
-    if(next >= strlen(src) || ischar(src[next], "|&<>")){
-        printf("Error: no input file\n");
-        return false;
-    }
-
-    return true;
-}
-
-bool check_output_sign(const char *src, int index)
-{
-    int prev = get_prev_nonspace_char_pos(src, index);
-    int next = get_next_nonspace_char_pos(src, index);
-
-    if(prev < 0) {
-        printf("Error: missing command\n");
-        return false;
-    }
-
-    if(next >= strlen(src) || ischar(src[next], "|&<>")){
-        printf("Error: no output file\n");
-        return false;
-    }
-
-    return true;
-}
-
-bool check_ampersand(const char *src, int index) {
-    int next = get_next_nonspace_char_pos(src, index);
-
-    if(next >= strlen(src))
-        return true;
-
-    printf("Error: mislocated background sign\n");
-    return false;
-}
-
-/*
- * Parse the source string
- * If source string is invalid,
- * we print error message and return false.
- * If source string is vaild,
- * we divide it into different commands and
- * call check_and_init_cmd function to
- * build cmds
- */
-bool parse_src_string_and_build_cmds(const char *src, singalcmd **cmds, int *cmdnum)
-{
-    char *delimiters = "|&<>";
-
-    char *inputfile = malloc(strlen(src) * sizeof(char));
-    char *outputfile = malloc(strlen(src) * sizeof(char));
-
-    char *arg = malloc((strlen(src) + 1) * sizeof(char));
-
-    char **args = malloc(strlen(src) * sizeof(char*));
-    for (int i = 0; i < strlen(src); ++i)
-        args[i] = malloc((strlen(src)+1) * sizeof(char));
-
-    (*cmdnum) = 0;
-    int _argsindex = 0;
-    int preindex = -1;
-    int argindex = 0;
-    bool input_redirect = false;
-    bool output_redirect = false;
-    bool background = false;
-
-    while(preindex < strlen(src)) {
-        int index = get_next_nonspace_char_pos(src, preindex);
-        if(index >= strlen(src))
-            break;
-
-        if(((index > preindex + 1) || ischar(src[index], delimiters)) && argindex != 0){
-            arg[argindex] = '\0';
-
-            if(input_redirect){
-                strcpy(inputfile, arg);
-                input_redirect = false;
-            } else if(output_redirect){
-                strcpy(outputfile, arg);
-                output_redirect = false;
-            }else {
-                strcpy(args[_argsindex], arg);
-                ++_argsindex;
+        if (pid == 0) {
+            if(execvp(iter->args[0], iter->args) == -1){
+                fprintf(stderr, "Error: command not found\n");
+                iter->status = 1;
+                exit(EXIT_FAILURE);
             }
-
-            memset(arg, 0, (strlen(src) + 1) * sizeof(char));
-            argindex = 0;
-        } else if((index == preindex + 1) && !ischar(src[index], delimiters)){
-            arg[argindex] = src[index];
-            ++argindex;
+        } else if(pid > 0){
+            wait(&status);
+            iter->status = status;
         }
-
-        switch(src[index]) {
-            case '|':
-                if(!check_vertical_bar(src, index))
-                    return false;
-                if(!check_command_validity(inputfile, outputfile, (*cmdnum) == 0, false, _argsindex))
-                    return false;
-                initialize_command(&(cmds[*cmdnum]), args, inputfile, outputfile, background, (*cmdnum) == 0, false, _argsindex);
-                // back to the origin state
-                ++(*cmdnum);
-                _argsindex = 0;
-                input_redirect = false;
-                output_redirect = false;
-                background = false;
-                memset(inputfile, 0, strlen(src) * sizeof(char));
-                memset(outputfile, 0, strlen(src) * sizeof(char));
-                for (int i = 0; i < strlen(src); ++i)
-                    memset(args[i], 0, (strlen(src)+1) * sizeof(char));
-                break;
-            case '<':
-                if(!check_input_sign(src, index))
-                    return false;
-                input_redirect = true;
-                break;
-            case '>':
-                if(!check_output_sign(src, index))
-                    return false;
-                output_redirect = true;
-                break;
-            case '&':
-                if(!check_ampersand(src, index))
-                    return false;
-                background = true;
-                break;
-            default:
-                break;
-        }
-        preindex = index;
     }
 
-    if(!check_command_validity(inputfile, outputfile, (*cmdnum) == 0, true, _argsindex))
+}
+
+/*
+ * read a single line from stdin
+ */
+bool readline(char *src) {
+    size_t size = MAX_SIZE;
+    __ssize_t charsnum = getline(&src, &size, stdin);
+    // no input or error
+    if(charsnum <= 1)
         return false;
-    initialize_command(&(cmds[*cmdnum]), args, inputfile, outputfile, background, (*cmdnum) == 0, true, _argsindex);
-    ++(*cmdnum);
-
-    free(inputfile);
-    free(outputfile);
-    for (int i = 0; i < strlen(src); ++i)
-        free(args[i]);
-    free(args);
-
+    // replace '\n' with '\0'
+    src[charsnum-1] = '\0';
     return true;
+}
+
+/*
+ * output execute results
+ * if exit true, output bye
+ * if exit false, output execute results
+ */
+void output(const char *src, const command *header) {
+    fprintf(stderr, "+ completed '%s' ", src);
+    for (const command *it = header; it != NULL ; it = it->next) {
+        fprintf(stderr, "[%d]", it->status);
+    }
+    fprintf(stderr, "\n");
 }
 
 int main(int argc, char *argv[])
 {
     char *src = malloc(MAX_SIZE * sizeof(char)); // FREE
-    //char **args = malloc(MAX_SIZE * sizeof(char*)); // FREE
+    command *header = NULL;
 
-    singalcmd **cmds = malloc(MAX_SIZE * sizeof(singalcmd*));
-    int cmdnum;
-
+    // this loop only exit if command is "exit"
     do {
         printf("sshell$ ");
 
-        size_t size = MAX_SIZE;
-        __ssize_t charsnum = getline(&src, &size, stdin);
-        if(charsnum == -1) // no input or error
-            continue;
-        src[charsnum-1] = '\0'; // delete trailing newline char
-
-        if(!parse_src_string_and_build_cmds(src, cmds, &cmdnum))
+        if(!readline(src))
             continue;
 
-        assert(cmds);
-        for (int i = 0; i < cmdnum; ++i) {
-            if(strcmp(cmds[i]->args[0], "exit") == 0)
-                goto label;
+        if(!parse_src_string(src, &header))
+            continue;
 
-            int pid = fork();
-            int status;
+        //if command is "exit", it will exit from this function
+        execute_commands(header, NULL);
 
-            if (pid == 0)
-                execvp(cmds[i]->args[0], cmds[i]->args);
-            else {
-                wait(&status);
-                fprintf(stderr, "Return status value for '%s': %d\n", src, WEXITSTATUS(status));
-            }
-        }
-        myfree(cmds, cmdnum);
-        continue;
-        /*
-         * I know it is ugly.
-         * Laugh me if you have better solution. :)
-         */
-        label:
-            myfree(cmds, cmdnum);
-            break;
+        output(src, header);
+        myfree(header);
+        header = NULL;
     } while(true);
-
-    free(src);
-    free(cmds);
-
-    fprintf(stderr, "Bye...\n");
-    return EXIT_SUCCESS;
 }
